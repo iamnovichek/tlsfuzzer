29061dba0ab92c415a6d39eb74175a70d0f0c3c2
Cucina <60839899+iamnovichek@users.noreply.github.com>
2024-04-09 10:39:58 +0200

    Update test-ccs.py

--- scripts/test-ccs.py
+++ scripts/test-ccs.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import traceback
 import sys
 import getopt
+import time
 from itertools import chain
 from random import sample
 
@@ -26,6 +27,7 @@ from tlslite.extensions import SupportedGroupsExtension, \
 from tlsfuzzer.utils.lists import natural_sort_keys
 from tlsfuzzer.helpers import SIG_ALL, AutoEmptyExtension
 
+from constants import CHARACTERS_LENGTH
 
 version = 1
 
@@ -65,6 +67,13 @@ def main():
     dhe = False
     ciphers = None
     ems = False
+
+    print("=" * CHARACTERS_LENGTH)
+    print("Basic conversation script; check basic communication with typical".upper())
+    print("cipher, TLS 1.2 or earlier and RSA key exchange (or (EC)DHE if".upper())
+    print("-d option is used)".upper())
+    print("=" * CHARACTERS_LENGTH)
+    time.sleep(3)
 
     argv = sys.argv[1:]
     opts, args = getopt.getopt(argv, "h:p:e:x:X:n:dC:M", ["help", "ems"])
@@ -220,7 +229,8 @@ def main():
     ordered_tests = chain(sanity_tests, sampled_tests, sanity_tests)
 
     for c_name, c_test in ordered_tests:
-        print("{0} ...".format(c_name))
+        print("{} -->\n".format(c_name).upper())
+        time.sleep(1)
 
         runner = Runner(c_test)
 
@@ -256,15 +266,13 @@ def main():
             else:
                 bad += 1
                 failed.append(c_name)
-
-    print("Basic conversation script; check basic communication with typical")
-    print("cipher, TLS 1.2 or earlier and RSA key exchange (or (EC)DHE if")
-    print("-d option is used)\n")
+        
+        print("=" * CHARACTERS_LENGTH, "\n")
 
     print("Test end")
     print(20 * '=')
     print("version: {0}".format(version))
-    print(20 * '=')
+    print(20 * '=', '\n')
     print("TOTAL: {0}".format(len(sampled_tests) + 2*len(sanity_tests)))
     print("SKIP: {0}".format(len(run_exclude.intersection(conversations.keys()))))
     print("PASS: {0}".format(good))
