99fa1a33f1596d466c14b13ac356de6d78978cb6
iamnovichek <rungotika@gmail.com>
2024-04-10 11:14:23 +0000

    main.py added

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import logging
+
+logger = logging.getLogger("tester")
+logger.setLevel(logging.INFO)
+
+terminal = logging.StreamHandler()
+terminal.setLevel(logging.INFO)
+
+logger.addHandler(terminal)
+
+
+def get_test_name(file_name):
+    fname = file_name.split(".")[0].split("test-")[1]
+
+    return " ".join(fname.split("-")).upper()
+
+
+def run_tests(tests_list, website_name):
+    for test in tests_list:
+        text = "\n" + ("-" * 50) + "\n" + " TESTING " + get_test_name(test) + "\n" + ("-" * 50)
+        logger.info(text)
+
+        os.system("PYTHONPATH=. python3 scripts/" + test + " -h " + website_name + " -p 443")
+
+
+if __name__ == "__main__":
+    all_files = os.listdir("./scripts/")
+
+    tests = [file for file in all_files if "test" in file]
+
+    website_name = input("Enter your website domain name: \n")
+
+    run_tests(tests, website_name)
+
